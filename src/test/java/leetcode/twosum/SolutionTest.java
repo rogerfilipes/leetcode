8b32d3ae56586a2275874a82aefc6eba430509a9
rogeriomoreira <rogerfilipes@gmail.com>
2022-11-03 11:38:11 +0000

    1. Two Sum 2. Add Two Numbers 3. Longest Substring Without Repeating Characters 11. Container With Most Water 15. 3Sum

---
+++ src/test/java/leetcode/twosum/SolutionTest.java
@@ -0,0 +1,40 @@
+package leetcode.twosum;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class SolutionTest {
+    @Test
+    void example1() {
+        int[] input = {2,7,11,15};
+        int target = 9;
+        int[] expected = {0,1};
+
+        int[] actual = new Solution().twoSum(input, target);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void example2() {
+        int[] input = {3,2,4};
+        int target = 6;
+        int[] expected = {1,2};
+
+        int[] actual = new Solution().twoSum(input, target);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void example3() {
+        int[] input = {3,3};
+        int target = 6;
+        int[] expected = {0,1};
+
+        int[] actual = new Solution().twoSum(input, target);
+
+        assertArrayEquals(expected, actual);
+    }
+}
