5fe595969f587a05fbb9839114505f0ac9056cf4
rogeriomoreira <rogerfilipes@gmail.com>
2022-11-03 12:15:50 +0000

    121. Best Time to Buy and Sell Stock

---
+++ src/test/java/leetcode/besttimebuysellstock/SolutionTest.java
@@ -0,0 +1,25 @@
+package leetcode.besttimebuysellstock;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+    @Test
+    void example1(){
+        int[] input = {7,1,5,3,6,4};
+        int expected = 5;
+
+        int output = new Solution().maxProfit(input);
+        assertEquals(expected, output);
+    }
+
+    @Test
+    void example2(){
+        int[] input = {7,6,4,3,1};
+        int expected = 0;
+
+        int output = new Solution().maxProfit(input);
+        assertEquals(expected, output);
+    }
+}
